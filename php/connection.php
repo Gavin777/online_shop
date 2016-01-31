5a99d97af4ed5fbf18c89e89489895630fe983ef
jonathanhchiu <jonathanchiu95@gmail.com>
2016-01-30 22:37:16 -0800

    testing ipn v3

--- php/connection.php
+++ php/connection.php
@@ -1,15 +1,11 @@
 <?php
 
 //server that your DB is on
-//$host = 'us-cdbr-iron-east-03.cleardb.net';
-//$database = 'heroku_835136c3b0ec6dc';
-//$username = 'bf2e5547b3763a';
-//$password = '7cd482e5';
+$host = 'us-cdbr-iron-east-03.cleardb.net';
+$database = 'heroku_835136c3b0ec6dc';
+$username = 'bf2e5547b3763a';
+$password = '7cd482e5';
 
-$host = 'localhost';
-$database = 'jonathandb';
-$username = 'root';
-$password = '';
 
 
 $link = mysqli_connect($host, $username, $password, $database);
